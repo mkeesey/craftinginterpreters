a7c3d0c1d317e803e6987d5dbec00fcd7e5fa10e
Mike Keesey <michael.w.keesey@gmail.com>
2024-06-24 17:07:24 -0600

    In progress

--- ast/ast.go
+++ ast/ast.go
@@ -1,3 +1,41 @@
 package ast
 
 //go:generate go run ../bin/genast/genast.go .
+
+// type PrintVisitor struct {
+// 	builder strings.Builder
+// }
+
+// func (p *PrintVisitor) Print(e Expr) string {
+// 	return e.Accept(p)
+// }
+
+// func (p *PrintVisitor) VisitBinary(e *Binary) Visitor {
+// 	return p
+// }
+
+// func (p *PrintVisitor) VisitGrouping(e *Grouping) Visitor {
+// 	return p
+// }
+
+// func (p *PrintVisitor) VisitLiteral(e *Literal) Visitor {
+// 	return p
+// }
+
+// func (p *PrintVisitor) VisitUnary(e *Unary) Visitor {
+// 	return p
+// }
+
+// func (p *PrintVisitor) parenthesize(name string, expr ...Expr) string {
+// 	p.builder.Reset()
+
+// 	p.builder.WriteString("(")
+// 	p.builder.WriteString(name)
+// 	for _, e := range expr {
+// 		p.builder.WriteString(" ")
+// 		p.builder.WriteString(e.Accept(p).(string))
+// 	}
+// 	p.builder.WriteString(")")
+
+// 	return p.builder.String()
+// }
